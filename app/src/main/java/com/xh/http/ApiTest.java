0b66b2835fb80042c40205c100b5e74d5781993d
liuhuiliang <825378291@qq.com>
2022-11-06 19:09:27 +0800

    a

--- app/src/main/java/com/xh/http/ApiTest.java
+++ app/src/main/java/com/xh/http/ApiTest.java
@@ -2,24 +2,14 @@ package com.xh.http;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiTest {
-    @POST("api/front/order/create")
-    Call<Object> create(@Body Entity entity);
+    @GET("json?lang=zh-C")
+    Call<Object> create();
 
-    public static class Entity {
-        public String app_id = "mobybox";
-        public String channel = "google";
-        public int coin = 0;
-        public String currency = "USD";
-        public String order_ext = "{\"order_id\":\"20220921055432970052\"}";
-        public String os = "android_1.0.0";
-        public String pay_channel = "google";
-        public int price = 5;
-        public String product_desc = "product_desc";
-        public String product_id = "com.mobybox.android.diamond1";
-        public String token = "gmd:tk:ZjU2MmVlNmEtMzk2MS0xMWVkLWIxZDAtZmExNjNlYTU1Y2U4LjEwMDA1OC4xNjYzNzMyODQwOTg2NTg3MTMy";
-        public int uid = 100058;
-    }
+
+
+
 }
--- app/src/main/java/com/xh/http/MainActivity.java
+++ app/src/main/java/com/xh/http/MainActivity.java
@@ -1,10 +1,16 @@
 package com.xh.http;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 
+
+import java.io.File;
 import java.io.IOException;
 
 import retrofit2.Call;
@@ -13,28 +19,39 @@ import retrofit2.Response;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            handler.sendEmptyMessageDelayed(0, 1000);
+        }
+    };
+    ApiTest test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        System.getProperties().put("http.proxyHost", "192.168.1.5");
+//        System.getProperties().put("http.proxyPort", 8888);
+//        System.getProperties().put("https.proxyHost", "192.168.1.5");
+//        System.getProperties().put("https.proxyPort", 8888);
         setContentView(R.layout.activity_main);
-        ApiTest apiTest = new RetrofitOkhttpBuilder("https://pay.imobybox.com")
-                .setCache(getApplicationContext())
-                .setFactory(GsonConverterFactory.create()).build(ApiTest.class);
-        try {
-            apiTest.create(new ApiTest.Entity()).enqueue(new Callback<Object>() {
+        RetrofitOkhttpBuilder builder = new RetrofitOkhttpBuilder("http://ip-api.com");
+        test = builder.setFactory(GsonConverterFactory.create()).build(ApiTest.class);
+        findViewById(R.id.test).setOnClickListener((v) -> {
+            test.create().enqueue(new Callback<Object>() {
                 @Override
                 public void onResponse(Call<Object> call, Response<Object> response) {
-                    Log.e("=====","response");
+
                 }
 
                 @Override
                 public void onFailure(Call<Object> call, Throwable t) {
-                    Log.e("======","",t);
+
                 }
             });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        });
+
+
     }
 }
